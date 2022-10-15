006136f9b0d52f7edb9b00063063a386b48d75af
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-10-15 14:38:50 +0300

    Done

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch(`https://anapioficeandfire.com/api/books`)
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json){
+   return renderBooks(json)
+    })
+  }
   
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
